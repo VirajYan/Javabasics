28708da7b1e1f9f9b9ee6fc60acf3d2ab712cbcd
VirajYan <virrrajyan@gmail.com>
2025-04-09 17:53:20 +0530

    initial commit

---
+++ DataTypes.java
@@ -0,0 +1,33 @@
+/*
+
+1) Primitive:
+short, int , long, float, double, char, boolean
+
+2) Non Primitive:
+string, class, array, interface, list, hashmap
+
+*/
+public class DataTypes {
+    public static void main(String[] args) {
+        short a = 1; // can hold 5 digits
+        System.out.println(a);
+
+        int b = 10000; // can hold 10 digits
+        System.out.println(b);
+
+        long c = 100000000000L; // can hold 19 digits
+        System.out.println(c);
+
+        float d = 1.4f; // 6/7 digits
+        System.out.println(d);
+
+        double e = 22.44; // 15/16 digits
+        System.out.println(e);
+
+        char ch = 'a';
+        System.out.println(ch);
+
+        boolean f = true;
+        System.out.println(f);
+    }
+}
