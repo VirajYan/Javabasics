cc69ad7bfb2cbd6c4cfdb4f1a12ffe9640a1c5df
VirajYan <virrrajyan@gmail.com>
2025-04-10 04:03:47 +0530

    remaining

--- Arrays.java
+++ Arrays.java
@@ -13,5 +13,12 @@ public class Arrays {
 
         int[] roll = new int[3];
         System.out.println(roll[2]); // since we havent initlaised it, it auto says 0
+
+        int[][] phoneNumbers = { { 99 }, { 80 } };
+        for (int i = 0; i < phoneNumbers.length; i++) {
+            for (int j = 0; j < phoneNumbers[0].length; j++) {
+                System.out.println(phoneNumbers[i][j]);
+            }
+        }
     }
 }
