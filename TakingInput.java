642b1702398ffcf4ea26f2aa86d902b231775489
VirajYan <virrrajyan@gmail.com>
2025-04-19 12:05:24 +0530

    input

---
+++ TakingInput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TakingInput {
+    public static void main(String[] args) {
+        int a;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter val of a: ");
+        a = sc.nextInt();
+        System.out.println("Value of a is: " + a);
+    }
+}
